95d947cb07f2b419f595daa5d8f29a6cbc6b75ee
dylan belouis <dylan.belouis@gmail.com>
2019-10-08 10:50:08 +0200

    ajout des articles

--- my-app/src/components/QuoteCard.js
+++ my-app/src/components/QuoteCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './QuoteCard.css';
 
 function QuoteCard () {
     return (
-<div ClassName="QuoteCard">
+<figure class="QuoteCard">
 <img
   src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
   alt="Nelson Muntz" />
@@ -13,7 +13,7 @@ function QuoteCard () {
   </blockquote>
   <cite>Nelson Muntz</cite>
 </figcaption>
-</div>
+</figure>
 );
 }
 export default QuoteCard;
