8caad475ea48dee9a7889ebcf56ec66785fb9506
dylan belouis <dylan.belouis@gmail.com>
2019-10-08 11:23:45 +0200

    ajouts de props

--- my-app/src/components/QuoteCard.js
+++ my-app/src/components/QuoteCard.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './QuoteCard.css';
 
-function QuoteCard () {
+function QuoteCard (props) {
     return (
 <figure class="QuoteCard">
 <img
-  src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
+  src={props.image}
   alt="Nelson Muntz" />
 <figcaption>
   <blockquote>
-    Shoplifting is a victimless crime, like punching someone in the dark.
+   {props.quote}
   </blockquote>
-  <cite>Nelson Muntz</cite>
+  <cite>{props.character}</cite>
 </figcaption>
 </figure>
 );
 }
+
 export default QuoteCard;
