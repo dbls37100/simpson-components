4bee94c1df35edee37a43c42c5c7e2ee9b81a0a9
dylan belouis <dylan.belouis@gmail.com>
2019-10-08 10:20:03 +0200

    navbar fonctionnel

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+      <div className="Navbar">
+        <nav><a href="https://fr.wikipedia.org/wiki/Les_Simpson">Les Simpson</a></nav>
+      </div>
+    );
+  }
+
+  export default Navbar;
